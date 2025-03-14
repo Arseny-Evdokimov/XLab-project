b342377989455f85a5eef50f90aa2bf03b9d5ac5
Arseny <a.evdokimov@carx-tech.com>
2025-03-14 14:50:11 +0300

    Unit 2.2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    class House
+    {
+        public string street;
+        public int number;
+
+        public void Print()
+        {
+            Console.WriteLine($"{street}, {number}");
+        }
+        static void Main(string[] args)
+        {
+            House tower = new House();
+            tower.street = "Садовая";
+            tower.number = 23;
+
+            tower.Print();
+        }
+    }
+}
